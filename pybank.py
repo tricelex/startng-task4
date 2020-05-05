1b0d66810a316327e755d096fc212405a90c9335
Chuckz Okoye <tricelex@gmail.com>
2020-05-05 14:39:37 +0100

    Initial commit

---
+++ pybank.py
@@ -0,0 +1,12 @@
+print("Welcome to PyBank!!")
+print("1 Staff Login")
+print("2 Close App\n")
+choice = int(input(''))
+running = True
+while running:
+	if choice == 2:
+		running = False
+		print('exit while loop')
+	print('Hello my name is chuckz')
+	choice = input('Do you want to quit  Y/N')
+
