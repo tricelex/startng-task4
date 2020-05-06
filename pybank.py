1fdad2154e3a4900b75d4597676b06844cc98f63
Chuckz Okoye <tricelex@gmail.com>
2020-05-06 07:40:14 +0100

    created multiple functons

--- pybank.py
+++ pybank.py
@@ -1,8 +1,8 @@
+import random
+
 print("Welcome to PyBank!!")
 print("1 Staff Login")
 print("2 Close App\n")
-choice = int(input(''))
-running = True
 
 
 def login():
@@ -27,10 +27,48 @@ def login():
 		login()
 
 
-while running:
-	if choice == 2:
-		running = False
-		print('exit while loop')
-		exit()
-	print('Hello my name is chuckz')
-	login()
+def create_bank_account():
+	print("Please Enter the following details to create an account")
+	account_name = input("Enter Account Name: ")
+	opening_balance = input("Enter Opening Balance: ")
+	account_type = input("Enter Account Type: ")
+	account_email = input("Enter Account Email: ")
+	account_number = generate_account_number()
+	user_info = {
+		'account_name': account_name,
+		'account_number': account_number,
+		'account_email': account_email,
+		'account_type': account_type,
+		'opening_balance': opening_balance
+	}
+
+	with open('customer.txt', 'w') as f:
+		f.write(str(user_info))
+	print(f'Account Number: {account_number}')
+
+
+def generate_account_number():
+	num = random.randint(1000000000, 9999999999)
+	return num
+
+
+def check_account_details():
+	with open('customer.txt', 'r') as f:
+		contents = eval(f.read())
+	acc_number = int(input('Please enter account number: '))
+	if contents['account_number'] == acc_number:
+		print(contents)
+	else:
+		print('You have entered an invalid account number')
+
+
+def logout():
+	pass
+
+
+def main():
+	pass
+
+
+# generate_account_number()
+check_account_details()
